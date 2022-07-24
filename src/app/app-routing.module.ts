78db1f8c8c064c26809082581b94aad941660fe2
George Kvirrik <gkvirika@gmail.com>
2022-07-25 00:48:51 +0400

    feat: sidebar menu and routing created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CasinoComponent } from './components/casino/casino.component';
+import { GameListComponent } from './components/game-list/game-list.component';
+import { LiveComponent } from './components/live/live.component';
+import { SportComponent } from './components/sport/sport.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'slots',
+    component: GameListComponent,
+  },
+  {
+    path: 'sport',
+    component: SportComponent,
+  },
+  {
+    path: 'live',
+    component: LiveComponent,
+  },
+  {
+    path: 'casino',
+    component: CasinoComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'slots',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { GameListComponent } from './components/game-list/game-list.component';
+import { CategoriesComponent } from './components/game-list/categories/categories.component';
+import { IconComponent } from './components/icon/icon.component';
+import { SportComponent } from './components/sport/sport.component';
+import { LiveComponent } from './components/live/live.component';
+import { CasinoComponent } from './components/casino/casino.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    GameListComponent,
+    CategoriesComponent,
+    IconComponent,
+    SportComponent,
+    LiveComponent,
+    CasinoComponent
   ],
   imports: [
     BrowserModule,
